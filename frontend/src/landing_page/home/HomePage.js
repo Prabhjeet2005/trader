953b6e8878873f4518ad13e5cf677cc808140077
Prabhjeet <prabhjeetsandhu010@gmail.com>
2024-12-26 07:07:04 +0530

    chore: Refactor HomePage component structure

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import Hero from './Hero';
-import Awards from './Awards';
-import Education from './Education';
-import Stats from './Stats';
-import Pricing from './Pricing';
-import OpenAccount from '../OpenAccount';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Education from "./Education";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 const HomePage = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Awards />
-      <Stats />
-      <Pricing />
-      <Education />
-      <OpenAccount />
-      <Footer />
-    </>
-  )
-}
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<Awards />
+			<Stats />
+			<Pricing />
+			<Education />
+			<OpenAccount />
+			<Footer />
+		</>
+	);
+};
 
-export default HomePage
+export default HomePage;
