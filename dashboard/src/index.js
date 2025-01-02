8620a023050501e9c15ac8dadfd0134c3d820095
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-01-02 20:47:15 +0530

    .

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -5,12 +5,12 @@ import {BrowserRouter,Route,Routes} from "react-router-dom"
 import Home from './Components/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <BrowserRouter>
       <Routes>
         <Route exact path='/*' element={<Home />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+
 );
 
